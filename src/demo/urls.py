2ce38012178503c55c652371649a72164e6cdfdf
Qi <shuaiqizju@gmail.com>
2022-08-02 00:44:52 +0800

    Update urls.py

--- src/demo/urls.py
+++ src/demo/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^get_survey$', views.get_survey, name='get_survey'),
     url(r'^automatic_taxonomy$', views.automatic_taxonomy, name='automatic_taxonomy'),
     url(r'^upload_refs$', views.upload_refs, name='upload_refs'),
+    url(r'^annotate_categories$', views.annotate_categories, name='annotate_categories'),
     url(r'^select_sections$', views.select_sections, name='select_sections'),
 ]
